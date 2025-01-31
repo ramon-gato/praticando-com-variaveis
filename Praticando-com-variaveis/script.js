3cca9f2f5737e39f0cd60c1bd16bf0e14a9697a0
Ramon Gato <103144822+ramon-gato@users.noreply.github.com>
2025-01-30 23:52:30 -0300

    praticando com variaveis

---
+++ Praticando-com-variaveis/script.js
@@ -0,0 +1,7 @@
+let nome = 'Ramon'
+let sobreNome = 'Gato'
+let anoNasciemnto = 1996
+let cidade = 'Manaus'
+let anoAtual = 2022
+
+console.log(`Eu sou ${nome} ${sobreNome} tenho ${anoAtual-anoNasciemnto} e moro em ${cidade}`)
